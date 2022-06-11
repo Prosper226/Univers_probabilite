718868432a9ceffd07d37a113c5cad6ef884e91a
Prosper226 <psedgo@gmail.com>
2022-06-11 11:44:22 +0000

    fix: add new composer

--- src/Arrangement.php
+++ src/Arrangement.php
@@ -1,7 +1,8 @@
 <?php
 
 /**
- * @author Prosper SEDGO
+ * @author BarkaLab
+ * @developer Prosper SEDGO
  * @created_at 10/06/2022
  * @last_update 11/06/2022
  * @github https://github.com/Prosper226?tab=repositories
